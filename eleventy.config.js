d45926ee3afa81913f16a1a870e6f59eae0f807c
Nandor Devai <mail@nandordevai.net>
2025-11-18 16:08:19 +0100

    simplify Youtube shortcode for no reason

--- eleventy.config.js
+++ eleventy.config.js
@@ -51,8 +51,8 @@ export default function (config) {
     });
 
     config.addShortcode('youtube', function(url) {
-        const [_, id] = url.split('=');
-        return `<iframe src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+        const src = url.replace('watch?v=', 'embed/');
+        return `<iframe src="${src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
     });
 
     config.addShortcode('excerpt', function(page) {
